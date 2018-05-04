409e20f30f44aa5147e0d7fcd01d80e8d0aae4e2
Robin Sola <rsolarms@gmail.com>
2018-05-04 12:15:37 -0700

    import nav-topics component to app.component html.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavTopicsComponent } from './nav-topics/nav-topics.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavTopicsComponent
   ],
   imports: [
     BrowserModule

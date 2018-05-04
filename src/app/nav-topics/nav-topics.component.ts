6bf90cf8cf870564f72f3637f3ca60e71d7f8d98
Robin Sola <rsolarms@gmail.com>
2018-05-04 12:14:06 -0700

    add nav-topics component folder with topics list in html and simple inline style in css for navbar look.

---
+++ src/app/nav-topics/nav-topics.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Story } from '../models/story.model';
+
+@Component({
+  selector: 'app-nav-topics',
+  templateUrl: './nav-topics.component.html',
+  styleUrls: ['./nav-topics.component.css']
+})
+export class NavTopicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

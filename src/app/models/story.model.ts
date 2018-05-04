7e608ebc3c92da29bf948d20b957bb07178cec29
Robin Sola <rsolarms@gmail.com>
2018-05-04 11:50:19 -0700

    add story constructor to new file story.model.ts.

---
+++ src/app/models/story.model.ts
@@ -0,0 +1,5 @@
+// each story will have IMAGE, but not sure how to incorporate that yet.
+
+export class Story {
+  constructor(public title: string, public author: string, public date: string, public article: string, public rating: number) { }
+}

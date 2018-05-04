9d0147d199a93b99a8cdaacedbce86fba8fc985f
Robin Sola <rsolarms@gmail.com>
2018-05-04 12:36:19 -0700

    add highlights component folder.

---
+++ src/app/highlights/highlights.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Story } from '../models/story.model';
+
+@Component({
+  selector: 'app-highlights',
+  templateUrl: './highlights.component.html',
+  styleUrls: ['./highlights.component.css']
+})
+export class HighlightsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

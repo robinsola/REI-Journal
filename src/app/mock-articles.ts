a7ba6fadd4685100baf3ecbd6871246657b1f9ba
Robin Sola <rsolarms@gmail.com>
2018-05-06 13:48:23 -0700

    create file for mock articles.

---
+++ src/app/mock-articles.ts
@@ -0,0 +1,7 @@
+import { Story } from './models/story.model';
+
+export const STORIES: Story[] = [
+  new Story('What does it take to set a fastest known time?', 'Liz Thomas', 'April 27, 2018', 'full-length article here', 5, 'user coments...', 1),
+  new Story('5 of the coolest mountain-town mayors', 'Joe Cutts', 'April 26, 2018', 'full-length article here', 5, 'user coments...', 2),
+  new Story('Tommy Caldwell talks public lands, his new film and parenting', 'Aer Parris', 'April 19, 2018', 'full-length article here', 5, 'user coments...', 3)
+];

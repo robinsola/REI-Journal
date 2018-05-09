6da83b340fc9e444762dd4cf1f6cef2e2c3c4ba5
Robin Sola <rsolarms@gmail.com>
2018-05-08 22:54:05 -0700

    add articles.

--- src/app/mock-articles.ts
+++ src/app/mock-articles.ts
@@ -6,5 +6,11 @@ export const STORIES: Story[] = [
   new Story('Tommy Caldwell talks public lands, his new film and parenting', 'Aer Parris', 'April 19, 2018', 'full-length article here', 5, 'highlight', 3),
   new Story('Explore How We gave back in 2017', 'REI Staff', 'May 4, 2018', 'full-length article here', 3, 'news', 4),
   new Story('Wild Ideas Worth Living', 'Shelby Stanger', 'May 2, 2018', 'full-length article here', 4, 'news', 5),
-  new Story('The Big Sur marathon is the most stunning costal run on Earth', 'Megan Michelson', 'April 30, 2018', 'full-length article here', 4, 'news', 6)
+  new Story('The Big Sur marathon is the most stunning costal run on Earth', 'Megan Michelson', 'April 30, 2018', 'full-length article here', 4, 'news', 6),
+  new Story('Everything you need for your first backpacking trip', 'Aer Parris', 'April 23, 2018', 'full-length article here', 4, 'gear', 7),
+  new Story('The best rain jackets of 2018', 'Aer Parris', 'April 16, 2018', 'full-length article here', 4, 'gear', 8),
+  new Story('What does moisture-wicking mean?', 'Ken Knapp', 'April 13, 2018', 'full-length article here', 2, 'gear', 9),
+  new Story('How much does it cost to hike the Pacific Crest Trail', 'Ken Knapp', 'May 1, 2018', 'full-length article here', 4, 'skills', 10),
+  new Story('How to ship a bike', 'REI Staff', 'April 3, 2018', 'full-length article here', 2, 'skills', 11),
+  new Story('How to have a more eco-friendly camping trip', 'Aer Parris', 'March 27, 2018', 'full-length article here', 4, 'skills', 12)
 ];

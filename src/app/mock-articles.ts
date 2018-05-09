05c99e93ad6b98894dceb88f2767609c846a8a37
Robin Sola <rsolarms@gmail.com>
2018-05-08 19:25:25 -0700

    add news stories to mock articles.

--- src/app/mock-articles.ts
+++ src/app/mock-articles.ts
@@ -5,3 +5,9 @@ export const STORIES: Story[] = [
   new Story('5 of the coolest mountain-town mayors', 'Joe Cutts', 'April 26, 2018', 'full-length article here', 5, 'user coments...', 2),
   new Story('Tommy Caldwell talks public lands, his new film and parenting', 'Aer Parris', 'April 19, 2018', 'full-length article here', 5, 'user coments...', 3)
 ];
+
+export const NEWSSTORIES: Story[] = [
+  new Story('Explore How We gave back in 2017', 'REI Staff', 'May 4, 2018', 'full-length article here', 3, 'user coments...', 4),
+  new Story('Wild Ideas Worth Living', 'Shelby Stanger', 'May 2, 2018', 'full-length article here', 4, 'user coments...', 5),
+  new Story('The Big Sur marathon is the most stunning costal run on Earth', 'Megan Michelson', 'April 30, 2018', 'full-length article here', 4, 'user coments...', 6),
+];

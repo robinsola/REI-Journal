d4ae9714d6c46ce25749bb491645b95fefe48584
Robin Sola <rsolarms@gmail.com>
2018-05-08 22:54:28 -0700

    delete commented out code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,10 @@ import { Story } from './models/story.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'REI CO-OP Journal';
-
-// the below sub-story lists will somehow need photos and teaser-text
-  // newsStoryList: Story[] = [
-  //   new Story('Explore How We gave back in 2017', 'REI Staff', 'May 4, 2018', 'full-length article here', 3, 'user coments...'),
-  //   new Story('Wild Ideas Worth Living', 'Shelby Stanger', 'May 2, 2018', 'full-length article here', 4, 'user coments...'),
-  //   new Story('The Big Sur marathon is the most stunning costal run on Earth', 'Megan Michelson', 'April 30, 2018', 'full-length article here', 4, 'user coments...'),
-  // ];
+  title = 'REI Co-op Journal';
+  // selectedStory = null;
   //
-  // gearStoryList: Story[] = [
-  //   new Story('Everything you need for your first backpacking trip', 'Aer Parris', 'April 23, 2018', 'full-length article here', 4, 'user coments...'),
-  //   new Story('The best rain jackets of 2018', 'Aer Parris', 'April 16, 2018', 'full-length article here', 4, 'user coments...'),
-  //   new Story('What does moisture-wicking mean?', 'Ken Knapp', 'April 13, 2018', 'full-length article here', 2, 'user coments...'),
-  // ];
-  //
-  // skillsStoryList: Story[] = [
-  //   new Story('How much does it cost to hike the Pacific Crest Trail', 'Ken Knapp', 'May 1, 2018', 'full-length article here', 4, 'user coments...'),
-  //   new Story('How to ship a bike', 'REI Staff', 'April 3, 2018', 'full-length article here', 2, 'user coments...'),
-  //   new Story('How to have a more eco-friendly camping trip', 'Aer Parris', 'March 27, 2018', 'full-length article here', 4, 'user coments...'),
-  // ];
-  selectedStory = null;
-
-  readStory(clickedStory) {
-    this.selectedStory = clickedStory;
-  }
-
-//addStory doesn't work -- research more on adding comments to articles using angular
-  // addStory(newStory: Story) {
-  //   this.selectedStory.push(newStory);
+  // readStory(clickedStory) {
+  //   this.selectedStory = clickedStory;
   // }
 }

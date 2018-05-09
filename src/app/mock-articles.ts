202d4b6003e39baf68d6d876e552059099fe91a3
Robin Sola <rsolarms@gmail.com>
2018-05-08 20:34:12 -0700

    change comments to category in Story constructor.

--- src/app/mock-articles.ts
+++ src/app/mock-articles.ts
@@ -1,13 +1,10 @@
 import { Story } from './models/story.model';
 
 export const STORIES: Story[] = [
-  new Story('What does it take to set a fastest known time?', 'Liz Thomas', 'April 27, 2018', 'full-length article here', 5, 'user coments...', 1),
-  new Story('5 of the coolest mountain-town mayors', 'Joe Cutts', 'April 26, 2018', 'full-length article here', 5, 'user coments...', 2),
-  new Story('Tommy Caldwell talks public lands, his new film and parenting', 'Aer Parris', 'April 19, 2018', 'full-length article here', 5, 'user coments...', 3)
-];
-
-export const NEWSSTORIES: Story[] = [
-  new Story('Explore How We gave back in 2017', 'REI Staff', 'May 4, 2018', 'full-length article here', 3, 'user coments...', 4),
-  new Story('Wild Ideas Worth Living', 'Shelby Stanger', 'May 2, 2018', 'full-length article here', 4, 'user coments...', 5),
-  new Story('The Big Sur marathon is the most stunning costal run on Earth', 'Megan Michelson', 'April 30, 2018', 'full-length article here', 4, 'user coments...', 6),
+  new Story('What does it take to set a fastest known time?', 'Liz Thomas', 'April 27, 2018', 'full-length article here', 5, 'highlight', 1),
+  new Story('5 of the coolest mountain-town mayors', 'Joe Cutts', 'April 26, 2018', 'full-length article here', 5, 'highlight', 2),
+  new Story('Tommy Caldwell talks public lands, his new film and parenting', 'Aer Parris', 'April 19, 2018', 'full-length article here', 5, 'highlight', 3),
+  new Story('Explore How We gave back in 2017', 'REI Staff', 'May 4, 2018', 'full-length article here', 3, 'news', 4),
+  new Story('Wild Ideas Worth Living', 'Shelby Stanger', 'May 2, 2018', 'full-length article here', 4, 'news', 5),
+  new Story('The Big Sur marathon is the most stunning costal run on Earth', 'Megan Michelson', 'April 30, 2018', 'full-length article here', 4, 'news', 6),
 ];

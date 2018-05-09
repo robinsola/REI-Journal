504123a2602e0fcaefe609e050be983a391f2a09
Robin Sola <rsolarms@gmail.com>
2018-05-08 20:02:16 -0700

    shorten variable name to stories.

--- src/app/highlights/highlights.component.ts
+++ src/app/highlights/highlights.component.ts
@@ -11,17 +11,18 @@ import { StoryService } from '../story.service';
 })
 
 export class HighlightsComponent implements OnInit {
-  @Input() childStoryList: Story[];
-  @Output() clickSender = new EventEmitter();
+    stories: Story[];
+    currentRoute: string = this.router.url;
+
 
   constructor(private router: Router, private storyService: StoryService){}
 
   ngOnInit() {
-    this.childStoryList = this.storyService.getStories()
+    this.stories = this.storyService.getStories()
   }
 
-  readBtnClicked(storyToRead: Story) {
-    this.router.navigate(['childStoryList', storyToRead.id]);
+  readDetails(clickedStory: Story) {
+    this.router.navigate(['stories', clickedStory.id]);
   };
 
 }

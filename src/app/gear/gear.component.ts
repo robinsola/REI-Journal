3e01fec41fcf085a2a8f4a56418edf053db04f22
Robin Sola <rsolarms@gmail.com>
2018-05-08 22:59:51 -0700

    import router, service.

--- src/app/gear/gear.component.ts
+++ src/app/gear/gear.component.ts
@@ -1,26 +1,26 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Story } from '../models/story.model';
+import { Router } from '@angular/router';
+import { StoryService } from '../story.service';
 
 @Component({
   selector: 'app-gear',
   templateUrl: './gear.component.html',
-  styleUrls: ['./gear.component.css']
+  styleUrls: ['./gear.component.css'],
+  providers: [StoryService]
 })
 
 export class GearComponent {
-  @Input() childStoryList: Story;
-  @Output() clickSender = new EventEmitter();
+  stories: Story[];
+  currentRoute: string = this.router.url;
+  
+  constructor(private router: Router, private storyService: StoryService){}
 
-  readBtnClicked(storyToRead: Story) {
-    this.clickSender.emit(storyToRead);
+  ngOnInit() {
+    this.stories = this.storyService.getStories()
   }
-}
 
-// export class GearComponent implements OnInit {
-//
-//   constructor() { }
-//
-//   ngOnInit() {
-//   }
-//
-// }
+  readDetails(clickedStory: Story) {
+    this.router.navigate(['stories', clickedStory.id]);
+  };
+}

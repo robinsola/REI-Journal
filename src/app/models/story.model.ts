f64269e1012bdc23bd53cd7ebf046e2740d80f06
Robin Sola <rsolarms@gmail.com>
2018-05-10 16:58:01 -0700

    add image to constructor.

--- src/app/models/story.model.ts
+++ src/app/models/story.model.ts
@@ -1,4 +1,4 @@
 export class Story {
   public rating: number
-  constructor(public title: string, public author: string, public date: string, public article: string, public category: string) { }
+  constructor(public title: string, public author: string, public date: string, public article: string, public category: string, imageUrl: string) { }
 }

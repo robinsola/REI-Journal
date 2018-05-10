054d0a3ec9b2abc691b8b79045209d85b813f787
Robin Sola <rsolarms@gmail.com>
2018-05-10 13:55:05 -0700

    delete id parameter.

--- src/app/models/story.model.ts
+++ src/app/models/story.model.ts
@@ -1,3 +1,3 @@
 export class Story {
-  constructor(public title: string, public author: string, public date: string, public article: string, public rating: number, public category: string, public id: number) { }
+  constructor(public title: string, public author: string, public date: string, public article: string, public rating: number, public category: string) { }
 }

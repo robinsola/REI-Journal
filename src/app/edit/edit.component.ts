64bfb758d5a5328cab0aea465470fd51cb58e19a
Robin Sola <rsolarms@gmail.com>
2018-05-11 12:06:35 -0700

    form to submit edits.

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Story } from '../models/story.model';
 
 @Component({
   selector: 'app-edit',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  @Input() selectedStory;
 
   constructor() { }
 
